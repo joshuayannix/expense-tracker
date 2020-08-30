dccc24a2579e0267212ed2d9d546345772655a40
joshuayannix <joshuayannix@gmail.com>
2020-08-30 10:30:59 -0400

    add item UI working

--- app.js
+++ app.js
@@ -12,8 +12,42 @@ const budgetController = (() => {
       entertainment: [],
       investments: [],
       other: [],
+    },
+    totals: {
+      housing: 0,
+      food: 0,
+      travel: 0,
+      entertainment: 0,
+      investments: 0,
+      other: 0,
     }
-  }
+  };
+
+  return {
+    addItem: (type, des, val) => {
+      let ID, newItem;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new Item
+      newItem = new Item(ID, des, val);
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+
+    testing: () => {
+      console.log(data)
+    }
+  };
 
 })();
 
@@ -25,7 +59,23 @@ const UIController = (() => {
         type: document.querySelector('.add__type').value,
         description: document.querySelector('.add__description').value,
         value: document.querySelector('.add__value').value,
-      }
+      };
+    },
+
+    addListItem: (obj, type) => {
+      let html = `
+        <div class="item clearfix" id="${type}-${obj.id}">
+          <div class="item__description">${obj.description}</div>
+          <div class="right clearfix">
+              <div class="item__value">${obj.value}</div>
+              <div class="item__delete">
+                  <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+              </div>
+          </div>
+        </div>
+      `
+      let listType = document.querySelector(`.${type}__list`)
+      listType.insertAdjacentHTML('beforeend', html)
     }
   }
 })();
@@ -47,8 +97,13 @@ const controller = ((budgetCtrl, UICtrl) => {
     let input = UICtrl.getInput();
 
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to the UI
+    UICtrl.addListItem(newItem, input.type)
+
     // 4. Calculate the budget
+
     // 5. Display the budget on the UI
 
   };
