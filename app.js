24b9c758d8484968491fe5b9cdc7c64ce836fe3a
joshuayannix <joshuayannix@gmail.com>
2020-08-29 23:31:32 -0400

    function constructor

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const budgetController = (() => {
+  const Item = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+  const data = {
+    allItems: {
+      housing: [],
+      food: [],
+      travel: [],
+      entertainment: [],
+      investments: [],
+      other: [],
+    }
+  }
 
 })();
+
 
 const UIController = (() => {
   return {
@@ -17,25 +33,33 @@ const UIController = (() => {
 // Global App Controller
 const controller = ((budgetCtrl, UICtrl) => {
 
+  const setupEventListeners = () => {
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', (event) => {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  }
   const ctrlAddItem = () => {
     // 1. Get the field input data
     let input = UICtrl.getInput();
-    console.log(input)
 
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
 
-    console.log('working')
+  };
+
+  return {
+    init: () => {
+      console.log('application started')
+      setupEventListeners();
+    }
   }
     
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', (event) => {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-
-  });
 })(budgetController, UIController);
+
+controller.init()
